4561da756101a541d2d4db7a72e0291bba362c01
hoa <hoahuy2606@gmail.com>
2023-03-16 08:41:17 +0700

    fix layout login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import "@/styles/globals.css";
 import "@/styles/style.css";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "@/redux/store";
 import LayoutPage from "@/components/layout/layout/Layout";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps, ...appProps }: AppProps) {
+  if ([`/login`].includes(appProps.router.pathname))
+    return (
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    );
   return (
     <Provider store={store}>
       <LayoutPage>
